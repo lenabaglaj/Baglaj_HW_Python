2062ed98977c8800d2178520af177767f0ec4a7c
lensbaglaj <lenabaglaj@yandex.ru>
2025-09-21 19:57:40 +0300

    01

---
+++ Lesson_1/lesson_1_task_5.py
@@ -0,0 +1,14 @@
+def print_let(num):
+    print(num, end="")
+    
+print_let("8")
+print_let("8")
+print_let("0")
+print_let("0")
+print_let("5")
+print_let("5")
+print_let("5")
+print_let("3")
+print_let("5")
+print_let("3")
+print_let("5")
